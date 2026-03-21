9f607d21dde337ce16c38939d82918e5d4871686
qawl987 <shiny861216@gmail.com>
2026-03-21 06:10:13 +0000

    feat: fix upf iterate all ip pool instead first one

--- controllers/nf/upf/templates.go
+++ controllers/nf/upf/templates.go
@@ -35,9 +35,11 @@ logger:
 dnnList:
 {{- range $netInstance := .N6cfg }}
   {{- range $dnn := $netInstance.DataNetworks }}
-  - cidr: {{(index $dnn.Pool 0).Prefix}}
+    {{- range $pool := $dnn.Pool }}
+  - cidr: {{ $pool.Prefix }}
     dnn: {{ $dnn.Name }}
     natifname: {{index $netInstance.Interfaces 0}}
+    {{- end }}
   {{- end }}
 {{- end}}
 
@@ -60,13 +62,17 @@ const wrapperScriptTemplateSource = `#!/bin/sh
 iptables -A FORWARD -j ACCEPT
 {{- range $netInstance := .N6cfg }}
   {{- range $dnn := $netInstance.DataNetworks }}
-iptables -t nat -A POSTROUTING -s {{(index $dnn.Pool 0).Prefix}} -o {{index $netInstance.Interfaces 0}} -j MASQUERADE  # route traffic comming from the UE  SUBNET to the interface N6
+    {{- range $pool := $dnn.Pool }}
+iptables -t nat -A POSTROUTING -s {{ $pool.Prefix }} -o {{index $netInstance.Interfaces 0}} -j MASQUERADE  # route traffic comming from the UE  SUBNET to the interface N6
+    {{- end }}
   {{- end }}
 {{- end }}
 echo "1200 n6if" >> /etc/iproute2/rt_tables # create a routing table for the interface N6
 {{- range $netInstance := .N6cfg }}
   {{- range $dnn := $netInstance.DataNetworks }}
-ip rule add from {{(index $dnn.Pool 0).Prefix}} table n6if   # use the created ip table to route the traffic comming from  the UE SUBNET
+    {{- range $pool := $dnn.Pool }}
+ip rule add from {{ $pool.Prefix }} table n6if   # use the created ip table to route the traffic comming from  the UE SUBNET
+    {{- end }}
 ip route add default via {{$.N6gw}} dev {{index $netInstance.Interfaces 0}} table n6if  # add a default route in the created table so  that all UEs will use this gateway for external communications (target IP not in the Data Network attached  to the interface N6) and then the Data Network will manage to route the traffic
   {{- end }}
 {{- end }}
